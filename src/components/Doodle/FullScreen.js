b44443d4fb01bf52f3ff041299eaa92420f50605
Wilson Hon <wilsongip@gmail.com>
2018-10-27 18:15:12 -0700

    Removed react

--- src/components/Doodle/FullScreen.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const FullScreen = styled.div`
-    width: 100vw;
-    height: 50%;
-`;
-
-export default FullScreen;
--- src/components/Matter/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Physics } from './Physics';
-export { default as Render } from './Render';
-export { default as ControlConstraint } from './ControlConstraint';
