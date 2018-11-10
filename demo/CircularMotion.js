6c2bf759e8539f2c58c35c85a4d8bda9bf4aec0e
Wilson Hon <wilsongip@gmail.com>
2018-11-09 21:12:40 -0800

    Added Changing Scenes and Equations Box update
    The index.html can now change to different scenes / physics concepts without having to go to a different page. It also update the equation <div> to its corresponding buttons

--- demo/CircularMotion.js
+++ demo/CircularMotion.js
@@ -1,91 +1,111 @@
-var canvas = document.getElementById('diagram');
+var Example = Example || {};
 
-var demVar = {
-    objects: []
-}
+Example.circularMotion = function(){
 
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
-	Body = Matter.Body;
-	Constraint = Matter.Constraint;
-	Mouse = Matter.Mouse;
-	MouseConstraint = Matter.MouseConstraint;
+	var canvas = document.getElementById('diagram');
 
-// create an engine
-var engine = Engine.create();
+	var demVar = {
+	    objects: []
+	}
 
-var render = Render.create({
-    element: canvas,
-    engine: engine,
-    options:
-    {
-        width: 700,
-        height: 400,
-        background: 'white',
-        wireframeBackground: '#222',
-        enabled: true,
-        wireframes: false,
-        showVelocity: true,
-        showAngleIndicator: true,
-        showCollisions: false,
-        pixelRatio: 1
-    }
-});
+	// module aliases
+	var Engine = Matter.Engine,
+	    Render = Matter.Render,
+	    World = Matter.World,
+	    Bodies = Matter.Bodies;
+		Body = Matter.Body;
+		Constraint = Matter.Constraint;
+		Mouse = Matter.Mouse;
+		MouseConstraint = Matter.MouseConstraint;
 
- // add mouse control
-var mouse = Mouse.create( render.canvas ),
-	mouseConstraint = MouseConstraint.create( engine, {
-		mouse: mouse,
-		constraint: 
-		{
-			// allow bodies on mouse to rotate
-			angularStiffness: 0,
-			render: {
-				visible: false
-			}
-		}
+	// create an engine
+	var engine = Engine.create();
+
+	var render = Render.create({
+	    element: canvas,
+	    engine: engine,
+	    options:
+	    {
+	        width: 700,
+	        height: 400,
+	        background: 'white',
+	        wireframeBackground: '#222',
+	        enabled: true,
+	        wireframes: false,
+	        showVelocity: true,
+	        showAngleIndicator: true,
+	        showCollisions: false,
+	        pixelRatio: 1
+	    }
 	});
 
-function createCircularMotion( radius )
-{
-	// add revolute constraint
-	demVar.objects.push( body = Bodies.circle( 400, 100, 25 ) );
+	 // add mouse control
+	var mouse = Mouse.create( render.canvas ),
+		mouseConstraint = MouseConstraint.create( engine, {
+			mouse: mouse,
+			constraint: {
+				// allow bodies on mouse to rotate
+				angularStiffness: 0,
+				render: {
+					visible: false
+				}
+			}
+		});
 
-	demVar.objects.push( constraint = Constraint.create( 
+	function createCircularMotion( radius )
 	{
-		pointA: { x: 350, y: 200 },
-		bodyB: body,
-		length: radius,
-		render: 
-		{
-			lineWidth: 5.5,
-			strokeStyle: '#666'
-		}
-	}) );
-	
-	World.add( engine.world, demVar.objects );
-}
+		// add revolute constraint
+		 demVar.objects.push( body = Bodies.circle( 400, 100, 50 ) );
+		
+		 demVar.objects.push( constraint = Constraint.create( {
+				pointA: { x: 350, y: 200 },
+				bodyB: body,
+				length: radius
+				render: 
+				{
+					lineWidth: 5.5,
+					strokeStyle: '#666'
+				}
+			} )
+		);
+		
+		World.add( engine.world, demVar.objects );
+	}
 
-createCircularMotion( 100 );
+	createCircularMotion( 100 );
 
-World.add( engine.world, mouseConstraint );
+	World.add( engine.world, mouseConstraint );
 
-// run the engine
-Engine.run( engine );
+	// run the engine
+	Engine.run( engine );
+	Render.run( render );
 
-Render.run( render );
+	document.getElementById('equations').innerHTML = `
+			<p>Equations</p>
+			
+			<div style="text-align: center">
+				Radius: <input type="text" id="radiusInput">
+				<button id="radius">Apply</button>
+			</div>
+	`;
 
-// Variables 
+	// Variables 
 
-document.getElementById("radius").onclick = function()
-{   
-	World.remove( engine.world, demVar.objects );
-	demVar.objects = [];
-	
-	radius = document.getElementById("radiusInput").value;
-	createCircularMotion( radius );
-}
+	document.getElementById("radius").onclick = function()
+	{   
+		World.remove( engine.world, demVar.objects );
+		demVar.objects = [];
+		
+		radius = document.getElementById("radiusInput").value;
+		createCircularMotion( radius );
+	}
 
+	    return {
+	        engine: engine,
+	        render: render,
+	        canvas: render.canvas,
+	        stop: function() {
+	            Matter.Render.stop(render);
+	        }
+	    };
+};
--- demo/universalGravitation.js
+++ demo/universalGravitation.js
@@ -1,125 +1,145 @@
-var canvas = document.getElementById('diagram');
+var Example = Example || {};
 
-var demVar = {
-    width: 700,
-    height: 400,
-    gravity: 0.001,
-    ballSize: 10,
-    friction: 0,
-    frictionStatic: 1,
-    frictionAir: 0,
-    restitution: 0.5,
-    velocityVector: true,
-    offset: 25,
-    lastTimeStamp: 0,
-    objects: []
-}
+Example.universalGravitation = function(){
+    var canvas = document.getElementById('diagram');
 
-function resetSettings(){
-    demVar.gravity = 0.001;
-    demVar.friction = 0;
-}
-
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Events = Matter.Events,
-    Bodies = Matter.Bodies;
-
-// create an engine
-var engine = Engine.create();
-
-var render = Render.create({
-    element: canvas,
-    engine: engine,
-    options:
-    {
-        width: demVar.width,
-        height: demVar.height,
-        background: 'white',
-        wireframeBackground: '#222',
-        enabled: true,
-        wireframes: false,
-        showVelocity: true,
-        showAngleIndicator: true,
-        showCollisions: false,
-        pixelRatio: 1
+    var demVar = {
+        width: 700,
+        height: 400,
+        gravity: 0.001,
+        ballSize: 10,
+        friction: 0,
+        frictionStatic: 1,
+        frictionAir: 0,
+        restitution: 0.5,
+        velocityVector: true,
+        offset: 25,
+        lastTimeStamp: 0,
+        objects: []
     }
-});
 
-function addObjectInEnviroment(x, y, r, sides, Vx, Vy){
-    var index = demVar.objects.length;
-    demVar.objects.push(
-        Bodies.polygon(x, y, sides,r, {
-            friction: demVar.friction,
-            frictionStatic: demVar.frictionStatic,
-            frictionAir: demVar.frictionAir,
-            restitution: demVar.restitution,
-            render: {
-                fillStyle: randomColor(),
-                strokeStyle: 'black',
-                lineWidth: 3
-            }
-        })
-    );
+    function resetSettings(){
+        demVar.gravity = 0.001;
+        demVar.friction = 0;
+    }
 
-    Matter.Body.setVelocity(demVar.objects[index], {
-        x: Vx,
-        y: Vy
-    });
-    World.add(engine.world, demVar.objects[index]);
-}
+    // module aliases
+    var Engine = Matter.Engine,
+        Render = Matter.Render,
+        World = Matter.World,
+        Events = Matter.Events,
+        Bodies = Matter.Bodies,
+        Runner = Matter.Runner;
 
-function createWall(x, y, width, height){
-    return Bodies.rectangle(x, y, width, height, {
-        isStatic: true,
-        render: {
-            restitution: 1,
-            fillStyle: 'white',
-            strokeStyle: 'black'
+
+
+    // create an engine
+    var engine = Engine.create();
+
+    var render = Render.create({
+        element: canvas,
+        engine: engine,
+        options:
+        {
+            width: demVar.width,
+            height: demVar.height,
+            background: 'white',
+            wireframeBackground: '#222',
+            enabled: true,
+            wireframes: false,
+            showVelocity: true,
+            showAngleIndicator: true,
+            showCollisions: false,
+            pixelRatio: 1
         }
     });
-}
 
-function simpleOrbit(){
-  resetSettings();
-  World.clear(engine.world, true);
-  demVar.objects = [];
-  addObjectInEnviroment(demVar.width*0.5, demVar.height*0.5, 50, 0, 0, 0);
-  addObjectInEnviroment(demVar.width*0.5-150, demVar.height*0.5, 10, 0, 0, 6);
-}
+    function addObjectInEnviroment(x, y, r, sides, Vx, Vy){
+        var index = demVar.objects.length;
+        demVar.objects.push(
+            Bodies.polygon(x, y, sides,r, {
+                friction: demVar.friction,
+                frictionStatic: demVar.frictionStatic,
+                frictionAir: demVar.frictionAir,
+                restitution: demVar.restitution,
+                render: {
+                    fillStyle: randomColor(),
+                    strokeStyle: 'black',
+                    lineWidth: 3
+                }
+            })
+        );
 
-function gravity() {
-  var length = demVar.objects.length;
-  for (var i = 1; i < length; i++) {
-    for (var j = 0; j < length; j++) {
-      if (i != j) {
-        var Dx = demVar.objects[j].position.x - demVar.objects[i].position.x;
-        var Dy = demVar.objects[j].position.y - demVar.objects[i].position.y;
-        var force = (engine.timing.timestamp-demVar.lastTimeStamp)*demVar.gravity * demVar.objects[j].mass * demVar.objects[i].mass / (Math.sqrt(Dx * Dx + Dy * Dy))
-        var angle = Math.atan2(Dy, Dx);
-        demVar.objects[i].force.x += force * Math.cos(angle);
-        demVar.objects[i].force.y += force * Math.sin(angle);
-      }
+        Matter.Body.setVelocity(demVar.objects[index], {
+            x: Vx,
+            y: Vy
+        });
+        World.add(engine.world, demVar.objects[index]);
     }
-  }
-  demVar.lastTimeStamp = engine.timing.timestamp;
-}
 
-engine.velocityIterations = 4;
-engine.positionIterations = 6;
-engine.world.gravity.y = 0;
+    function createWall(x, y, width, height){
+        return Bodies.rectangle(x, y, width, height, {
+            isStatic: true,
+            render: {
+                restitution: 1,
+                fillStyle: 'white',
+                strokeStyle: 'black'
+            }
+        });
+    }
 
-simpleOrbit()
+    function simpleOrbit(){
+      resetSettings();
+      World.clear(engine.world, true);
+      demVar.objects = [];
+      addObjectInEnviroment(demVar.width*0.5, demVar.height*0.5, 50, 0, 0, 0);
+      addObjectInEnviroment(demVar.width*0.5-150, demVar.height*0.5, 10, 0, 0, 6);
+    }
 
-World.add( engine.world, [] );
+    function gravity() {
+      var length = demVar.objects.length;
+      for (var i = 1; i < length; i++) {
+        for (var j = 0; j < length; j++) {
+          if (i != j) {
+            var Dx = demVar.objects[j].position.x - demVar.objects[i].position.x;
+            var Dy = demVar.objects[j].position.y - demVar.objects[i].position.y;
+            var force = (engine.timing.timestamp-demVar.lastTimeStamp)*demVar.gravity * demVar.objects[j].mass * demVar.objects[i].mass / (Math.sqrt(Dx * Dx + Dy * Dy))
+            var angle = Math.atan2(Dy, Dx);
+            demVar.objects[i].force.x += force * Math.cos(angle);
+            demVar.objects[i].force.y += force * Math.sin(angle);
+          }
+        }
+      }
+      demVar.lastTimeStamp = engine.timing.timestamp;
+    }
 
-Events.on( engine, "beforeTick", function(event) {
-  gravity();
-} );
+    engine.velocityIterations = 4;
+    engine.positionIterations = 6;
+    engine.world.gravity.y = 0;
 
-// run the engine
-Engine.run( engine );
+    simpleOrbit()
 
-Render.run( render );
+    World.add( engine.world, [] );
+
+    Events.on( engine, "beforeTick", function(event) {
+      gravity();
+    } );
+
+    // run the engine
+    Engine.run( engine );
+
+    Render.run( render );
+
+    document.getElementById('equations').innerHTML = `
+        <p>Equations</p>
+    `;
+
+    return {
+        engine: engine,
+        render: render,
+        canvas: render.canvas,
+        stop: function() {
+            Matter.Render.stop(render);
+        }
+    };
+};
