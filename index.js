4b03e4d7e0f5e19713fa2d8b3b317efd7f19274b
Wilson Hon <wilsongip@gmail.com>
2018-11-11 20:25:43 -0800

    Organize files and update express file

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+app.use("/demo", express.static(__dirname + '/demo'));
+app.use("/libraries", express.static(__dirname + '/libraries'));
+app.use("/styles", express.static(__dirname + '/styles'));
 
 app.get('/', (req, res) => {
-	res.send('Hello World')
-})
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
-app.listen(3000, () => console.log('Server running on port 3000')
+app.get('*', (req, res) => {
+	res.status(404).send('NO PAGE FOUND');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
