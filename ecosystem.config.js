7283f170d5418d2447f4547b1180d199b775a4cc
Wilson Hon <wilsongip@gmail.com>
2018-11-11 22:52:32 -0800

    testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: "ec2-user",
       host: "ec2-18-216-89-167.us-east-2.compute.amazonaws.com",
-      key: "D:\Users\Gippy\Downloads\physics2\\test/seephysics.pem",
+      key: "D:/Users/Gippy/Downloads/physics2/test/seephysics.pem",
       ref: "origin/master",
       repo: "git@github.com:TheCPPGang/PhysicsVisualization.git",
       path: "/home/ec2-user/server",
