ec7cdc883034c68f9f6fd6ba83c6493cf9ed35a8
Wilson Hon <wilsongip@gmail.com>
2018-11-11 20:35:44 -0800

    Add PM2 for cloning and restarting server locally

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'PhysicsVisualization',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: 'ec2-18-216-89-167.us-east-2.compute.amazonaws.com',
+      key: '../seephysics.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:TheCPPGang/PhysicsVisualization.git',
+      path: '/home/ec2-user/PhysicsVisualization',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
