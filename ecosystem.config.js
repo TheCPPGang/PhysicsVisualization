c8c032c020e667f35e0cb4ebf8e31fe6a9c7021b
Wilson Hon <wilsongip@gmail.com>
2018-11-12 21:49:32 -0800

    add back ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: "PhysicsVisualization",
+    script: "./index.js"
+  }],
+  deploy: {
+    production: {
+      user: "ec2-user",
+      host: "ec2-18-216-89-167.us-east-2.compute.amazonaws.com",
+      key: "D:/Users/Gippy/Downloads/physics2/test/seephysics.pem",
+      ref: "origin/master",
+      repo: "git@github.com:TheCPPGang/PhysicsVisualization.git",
+      path: "~/server",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+    }
+  }
+}
