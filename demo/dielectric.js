e4c70f6a5860bac2081d1325d338c10752813aa1
acprado2 <acprado@cpp.edu>
2018-11-20 14:20:08 -0800

    further work on dielectric demo

--- demo/dielectric.js
+++ demo/dielectric.js
@@ -1,161 +1,253 @@
 var Example = Example || {};
 
 Example.dielectric = function(){
+	var canvas = document.getElementById('diagram');
 
-var canvas = document.getElementById('diagram');
-
-var demVar = 
-{
-    objects: [],
-}
-
-// module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    World = Matter.World,
-    Bodies = Matter.Bodies,
-	Body = Matter.Body,
-	Composites = Matter.Composites;
-	
-// create an engine
-var engine = Engine.create();
-
-var render = Render.create({
-    element: canvas,
-    engine: engine,
-    options:
-    {
-        width: 700,
-        height: 400,
-        background: 'white',
-        wireframeBackground: '#222',
-        enabled: true,
-        wireframes: false,
-        showVelocity: false,
-        showAngleIndicator: false,
-        showCollisions: false,
-        pixelRatio: 1
-    }
-});
-
-// Define our categories (as bit fields, there are up to 32 available)
-var CAPACITOR_MASK = 0x0001,
-	CHARGE_MASK = 0x0002;
-	
-// Color
-var RED = '#FF0000',
-	BLUE = '#0000FF',
-	YELLOW = '#999900',
-	BLACK = '#000000';
-
-// Create a particle in the dielectric
-function createParticle( x, y ) 
-{
-		var plusA = Bodies.rectangle( x, y, 5, 1, { render: { fillStyle: RED } } ),
-			plusB = Bodies.rectangle( x, y,  1, 5, { render: { fillStyle: RED } } );
-			minus = createNegativeCharge( x + 10, y ),
-			particle = Bodies.circle( x + 5, y, 10, { render: { fillStyle: BLACK, opacity: .05 } } );
-
-        return Body.create({
-			isStatic: true,
-            parts: [plusA, plusB, minus, particle],
-			collisionFilter: { mask: CHARGE_MASK }
-        });
-}
-
-// Create a positive charge
-function createPositiveCharge( x, y )
-{
-	var plusA = Bodies.rectangle( x, y, 5, 1, { render: { fillStyle: RED } } ),
-		plusB = Bodies.rectangle( x, y,  1, 5, { render: { fillStyle: RED } } );
-
-	return Body.create({
-		isStatic: true,
-		parts: [plusA, plusB],
-		collisionFilter: { mask: CHARGE_MASK }
-	});
-}
-
-// Create a minus charge
-function createNegativeCharge( x, y )
-{
-	return Bodies.rectangle( x, y, 5, 1, 
-	{ 
-		isStatic: true,
-		render: { fillStyle: BLUE },
-		collisionFilter: { mask: CHARGE_MASK }
-	} );
-}
-
-// We need to create a circuit diagram and matter isn't really the ideal
-// engine for this sort of thing, but really thin, static boxes will probably
-// work fine for now.
-function createEnvironment()
-{
-	
-	// Left wall 
-	demVar.objects.push( Bodies.rectangle( 75, 225, 1, 250, { isStatic: true } ) );
-	
-	// Right wall 
-	demVar.objects.push( Bodies.rectangle( 600, 225, 1, 250, { isStatic: true } ) );
-	
-	// Top wall left side
-	demVar.objects.push( Bodies.rectangle( 175, 100, 200, 1, { isStatic: true } ) );
-	
-	// Top wall right side
-	demVar.objects.push( Bodies.rectangle( 500, 100, 200, 1, { isStatic: true } ) );
-	
-	// Bottom wall left side
-	demVar.objects.push( Bodies.rectangle( 200, 350, 250, 1, { isStatic: true } ) );
-	
-	// Bottom wall right side
-	demVar.objects.push( Bodies.rectangle( 475, 350, 250, 1, { isStatic: true } ) );
-	
-	// Battery +
-	demVar.objects.push( Bodies.rectangle( 325, 350, 1, 75, { isStatic: true } ) );
-	
-	// Battery -
-	demVar.objects.push( Bodies.rectangle( 350, 350, 1, 50, { isStatic: true } ) );
-	
-	// Left capacitor plate
-	demVar.objects.push( Bodies.rectangle( 400, 100, 20, 150, { isStatic: true, collisionFilter: { mask: CAPACITOR_MASK } } ) );
-	
-	// Right capacitor plate
-	demVar.objects.push( Bodies.rectangle( 275, 100, 20, 150, { isStatic: true, collisionFilter: { mask: CAPACITOR_MASK } } ) );
-	
-	// Dielectric
-	demVar.objects.push( Bodies.rectangle( 337, 100, 106, 150, 
+	var demVar = 
 	{
-		isStatic: true,
-		render: { fillStyle: YELLOW },
-		collisionFilter: { mask: CAPACITOR_MASK }
-	} ) );
-	
-	for ( var i = 0; i < 5; i++ )
-	{
-		for ( var j = 0; j < 7; j++ )
+		k: 1,
+		Area: 1,
+		distance: 1,
+		objects: [],
+		particles: [],
+		charges: [],
+		capacitor: [],
+	}
+
+	// module aliases
+	var Engine = Matter.Engine,
+		Render = Matter.Render,
+		World = Matter.World,
+		Bodies = Matter.Bodies,
+		Body = Matter.Body,
+		Composites = Matter.Composites;
+		
+	// create an engine
+	var engine = Engine.create();
+
+	var render = Render.create({
+		element: canvas,
+		engine: engine,
+		options:
 		{
-			demVar.objects.push( createParticle( 292 + ( 20 * i ), 40 + ( 20 * j ) ) );
+			width: 700,
+			height: 400,
+			background: 'white',
+			wireframeBackground: '#222',
+			enabled: true,
+			wireframes: false,
+			showVelocity: false,
+			showAngleIndicator: false,
+			showCollisions: false,
+			pixelRatio: 1
 		}
+	});
+
+	// Color
+	var RED = '#FF0000',
+		BLUE = '#0000FF',
+		YELLOW = '#999900',
+		BLACK = '#000000';
+
+	// Create a particle in the dielectric
+	function createParticle( x, y ) 
+	{
+		// Offset charge positions in the particle depending on the current value of k
+		var plusA = Bodies.rectangle( x + demVar.k , y, 12.5, 1, { render: { fillStyle: RED } } ),
+			plusB = Bodies.rectangle( x + demVar.k, y,  1, 12.5, { render: { fillStyle: RED } } ),
+			minus = createNegativeCharge( x - demVar.k, y, 12.5 ),
+			particle = Bodies.circle( x, y, 20, { render: { fillStyle: BLACK, opacity: .05 } } );
+
+		return Body.create({
+			isStatic: true,
+			parts: [plusA, plusB, minus, particle],
+		});
+	}
+
+	// Create a positive charge
+	function createPositiveCharge( x, y, length )
+	{
+		var plusA = Bodies.rectangle( x, y, length, 1, { render: { fillStyle: RED } } ),
+			plusB = Bodies.rectangle( x, y,  1, length, { render: { fillStyle: RED } } );
+
+		return Body.create({
+			isStatic: true,
+			parts: [plusA, plusB],
+		});
+	}
+
+	// Create a minus charge
+	function createNegativeCharge( x, y, length )
+	{
+		return Bodies.rectangle( x, y, length, 1, 
+		{ 
+			isStatic: true,
+			render: { fillStyle: BLUE },
+		} );
+	}
+
+	// create dielectric particle array and render in a 3x3 grid
+	function createParticleArray()
+	{
+		// clear particles
+		World.remove( engine.world, demVar.particles );
+		demVar.particles = [];
+		
+		for ( var i = 0; i < demVar.distance; i++ )
+		{
+			for ( var j = 0; j < 3; j++ )
+			{
+				demVar.particles.push( createParticle( 375 - ( 25 * demVar.distance ) + ( 50 * i ), 50 + ( 50 * j ) ) );
+			}
+		}
+		
+		World.add( engine.world, demVar.particles );
+	}
+
+	// create array of charges held by capacitor plates
+	function createChargeArray()
+	{
+		// clear charges
+		World.remove( engine.world, demVar.charges );
+		demVar.charges = [];
+		
+		for ( var i = 0; i < demVar.Area; i++ )
+		{
+			for ( var j = 0; j < 5; j++ )
+			{
+				// One charge for each plate		
+				//demVar.charges.push( createNegativeCharge( 425 - ( 25 * demVar.Area ) + ( 50 * i ), 40 + ( 30 * j ), 20 ) );
+				//demVar.charges.push( createPositiveCharge( 325 - ( 25 * demVar.Area ) + ( 50 * i ), 40 + ( 30 * j ), 20 ) );
+			}
+		}
+		
+		World.add( engine.world, demVar.charges );
 	}
 	
-	World.add( engine.world, demVar.objects );
-}
+	// When creating the capacitor the offset is adjusted based on the area of the plates as well as the 
+	// distance between the plates specified by the user.
+	// 1m = 25px
+	function createCapacitor()
+	{
+		World.remove( engine.world, demVar.capacitor );
+		demVar.capacitor = [];
+		
+		// Left capacitor plate
+		demVar.capacitor.push( leftPlate = Bodies.rectangle( 350 + ( 25 * demVar.Area )  + ( 25 * demVar.distance ), 100, 50 * demVar.Area, 150, 
+		{ 
+			isStatic: true,
+			render: { fillStyle: BLACK },  
+		} ) );
+		
+		// Right capacitor plate
+		demVar.capacitor.push( rightPlate = Bodies.rectangle( 350 - ( 25 * demVar.Area ) - ( 25 * demVar.distance ), 100, 50 * demVar.Area, 150, 
+		{ 
+			isStatic: true,
+			render: { fillStyle: BLACK }, 
+		} ) );
+		
+		// Dielectric
+		demVar.capacitor.push( dielectric = Bodies.rectangle( 350 , 100, 50 * demVar.distance, 150, 
+		{
+			isStatic: true,
+			render: { fillStyle: YELLOW },
+		} ) );
+		
+		World.add( engine.world, demVar.capacitor );
+	}
 
-createEnvironment();
+	// We need to create a circuit diagram and matter isn't really the ideal
+	// engine for this sort of thing, but really thin, static boxes will probably
+	// work fine for now.
+	function createEnvironment()
+	{
+		
+		// Left wall 
+		demVar.objects.push( Bodies.rectangle( 75, 225, 1, 250, { isStatic: true } ) );
+		
+		// Right wall 
+		demVar.objects.push( Bodies.rectangle( 625, 225, 1, 250, { isStatic: true } ) );
+		
+		// Top wall
+		demVar.objects.push( Bodies.rectangle( 350, 100, 550, 1, { isStatic: true } ) );
+		
+		// Bottom wall left side
+		demVar.objects.push( Bodies.rectangle( 205, 350, 262, 1, { isStatic: true } ) );
+		
+		// Bottom wall right side
+		demVar.objects.push( Bodies.rectangle( 495, 350, 262, 1, { isStatic: true } ) );
+		
+		// Battery +
+		demVar.objects.push( Bodies.rectangle( 337, 350, 1, 75, { isStatic: true } ) );
+		
+		// Battery -
+		demVar.objects.push( Bodies.rectangle( 363, 350, 1, 50, { isStatic: true } ) );
+		
+		World.add( engine.world, demVar.objects );
+		
+		createCapacitor();
+		createParticleArray();
+		createChargeArray();
+	}
 
-// run the engine
-Engine.run( engine );
+	createEnvironment();
 
-// run the renderer
-Render.run( render );
+	// run the engine
+	Engine.run( engine );
 
-return {
-	        engine: engine,
-	        render: render,
-	        canvas: render.canvas,
-	        stop: function() {
-	            Matter.Render.stop(render);
-	        }
-	    };
+	// run the renderer
+	Render.run( render );
+
+	document.getElementById('equations').innerHTML = `
+			<p>Equations</p>
+			
+			<div style="text-align: center">
+				Dielectric Constant: <input type="text" id="kInput">
+				<button id="kConstant">Apply</button>
+				
+				<br/><br/>
+				
+				Plate Area: <input type="text" id="areaInput">
+				<button id="area">Apply</button>
+				
+				<br/><br/>
+				
+				Distance Between Plates: <input type="text" id="distanceInput">
+				<button id="distance">Apply</button>
+			</div>
+	`;
+
+	// Variables 
+	
+	document.getElementById( "kConstant" ).onclick = function()
+	{   		
+		demVar.k = parseFloat( document.getElementById( "kInput" ).value );
+		createParticleArray();
+		createChargeArray();
+	}	
+
+	document.getElementById( "area" ).onclick = function()
+	{   		
+		demVar.Area = parseFloat( document.getElementById( "areaInput" ).value );
+		createCapacitor();
+		createParticleArray();
+		createChargeArray();
+	}	
+
+	document.getElementById( "distance" ).onclick = function()
+	{   		
+		demVar.distance = parseFloat( document.getElementById( "distanceInput" ).value );
+		createCapacitor();
+		createParticleArray();
+		createChargeArray();
+	}	
+
+    return {
+        engine: engine,
+        render: render,
+        canvas: render.canvas,
+        stop: function() {
+            Matter.Render.stop(render);
+        }
+    };
 };
